1a3f419d40a8b2e75d856127869f56eee674d320
Warrenn Enslin <warrennenslin@Warrenns-MacBook-Pro.local>
2020-02-20 10:17:26 +0200

    missed the obvious

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,14 @@
+#!/usr/bin/env bash
+set -e
+
 export ACCESS_KEY_ID=ACCESS_KEY_ID
 export SECRET_ACCESS_KEY=SECRET_ACCESS_KEY
 export PUBLISHABLE_KEY=PUBLISHABLE_KEY
 export SECRET_KEY=SECRET_KEY
-export DATABASE_URL=$(ssm get-parameter --name "/Config/Staging/vbios-platform/DATABASE_URL" --region us-east-1 --with-decryption --query 'Parameter.Value' --output text)
+export DATABASE_URL=$(aws ssm get-parameter --name "/Config/Staging/vbios-platform/DATABASE_URL" --region us-east-1 --with-decryption --query 'Parameter.Value' --output text)
 export RAILS_MASTER_KEY=RAILS_MASTER_KEY
 export SECRET_KEY_BASE=SECRET_KEY_BASE
 
 echo printenv
 
-python /tmp/application.py
+exec python /tmp/application.py
