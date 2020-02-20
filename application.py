a2a024501ed3f801ae7e241c6a96638d8c676bdf
Warrenn Enslin <warrennenslin@Warrenns-MacBook-Pro.local>
2020-02-20 06:53:49 +0200

    showing results[0]

--- application.py
+++ application.py
@@ -59,7 +59,7 @@ def application(environ, start_response):
             logger.warning('Error retrieving request body for async work.')
         response = ''
     else:
-        response = welcome.format(result)
+        response = welcome.format(result[0])
     status = '200 OK'
     headers = [('Content-type', 'text/html')]
 
