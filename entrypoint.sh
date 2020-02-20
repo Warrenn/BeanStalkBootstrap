4f59830435d4a17ece9af581d99edee28b7cfb98
Warrenn Enslin <warrennenslin@Warrenns-MacBook-Pro.local>
2020-02-20 03:41:07 +0200

    changed password

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@ export ACCESS_KEY_ID=ACCESS_KEY_ID
 export SECRET_ACCESS_KEY=SECRET_ACCESS_KEY
 export PUBLISHABLE_KEY=PUBLISHABLE_KEY
 export SECRET_KEY=SECRET_KEY
-export DATABASE_URL="postgresql://vbio:37wMEFUi7#1@vbios-platform-staging.cluster-ch7l2hgjekr1.us-east-1.rds.amazonaws.com/postgres?sslmode=require&sslrootcert=/etc/ssl/certs/rds-ca-2019-root.pem"
+export DATABASE_URL="postgresql://vbios:bcfhugmnHMW9h90V7Etg@vbios-platform-staging.cluster-ch7l2hgjekr1.us-east-1.rds.amazonaws.com/postgres?sslmode=require&sslrootcert=/etc/ssl/certs/rds-ca-2019-root.pem"
 export RAILS_MASTER_KEY=RAILS_MASTER_KEY
 export SECRET_KEY_BASE=SECRET_KEY_BASE
 
