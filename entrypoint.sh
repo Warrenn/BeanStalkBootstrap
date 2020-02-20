9c6b5af80c53b82fe6b74599162bc0a3dec84a09
Warrenn Enslin <warrennenslin@Warrenns-MacBook-Pro.local>
2020-02-20 14:45:43 +0200

    added some environment variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 set -e
 
-export DATABASE_URL=$(aws ssm get-parameter --name "/config/staging/vbios-platform/DATABASE_URL" --region us-east-1 --with-decryption --query 'Parameter.Value' --output text)
-export RAILS_MASTER_KEY=$(aws ssm get-parameter --name "/config/staging/vbios-platform/RAILS_MASTER_KEY" --region us-east-1 --with-decryption --query 'Parameter.Value' --output text)
-export SECRET_KEY_BASE=$(aws ssm get-parameter --name "/config/staging/vbios-platform/SECRET_KEY_BASE" --region us-east-1 --with-decryption --query 'Parameter.Value' --output text)
-export PUBLISHABLE_KEY=$(aws ssm get-parameter --name "/config/staging/vbios-platform/PUBLISHABLE_KEY" --region us-east-1 --with-decryption --query 'Parameter.Value' --output text)
-export SECRET_KEY=$(aws ssm get-parameter --name "/config/staging/vbios-platform/SECRET_KEY" --region us-east-1 --with-decryption --query 'Parameter.Value' --output text)
-export MAILGUN_API_KEY=$(aws ssm get-parameter --name "/config/staging/vbios-platform/MAILGUN_API_KEY" --region us-east-1 --with-decryption --query 'Parameter.Value' --output text)
+get_parameter() {
+    echo "$(aws ssm get-parameter --name "/config/$RAILS_ENV/vbios-platform/$1" --region us-east-1 --with-decryption --query 'Parameter.Value' --output text)"
+}
+
+export DATABASE_URL=$(get_parameter "DATABASE_URL")
+export RAILS_MASTER_KEY=$(get_parameter "RAILS_MASTER_KEY")
+export SECRET_KEY_BASE=$(get_parameter "SECRET_KEY_BASE")
+export PUBLISHABLE_KEY=$(get_parameter "PUBLISHABLE_KEY")
+export SECRET_KEY=$(get_parameter "SECRET_KEY")
+export MAILGUN_API_KEY=$(get_parameter "MAILGUN_API_KEY")
 
 printenv >> '/tmp/sample-app/printenv-app.log'
 
