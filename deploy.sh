72bf0a45ad271df268dc704ec0ac59c113a5460b
Warrenn Enslin <warrennenslin@Warrenns-MacBook-Pro.local>
2020-02-23 21:27:22 +0200

    updated environment

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ set -e
 
 export PATH="/root/.ebcli-virtual-env/executables:$PATH"
 
-eb deploy $ENVIRONMENT
-
 aws elasticbeanstalk update-environment --environment-name $ENVIRONMENT --application-name $APPLICATION --region $AWS_REGION --option-settings file://$PWD/.optionsettings/$ENVIRONMENT.json
+
+eb deploy $ENVIRONMENT
