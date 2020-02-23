33395f801ac5e129cb2d4845186d2edeee0e3a46
Warrenn Enslin <warrennenslin@Warrenns-MacBook-Pro.local>
2020-02-23 20:09:22 +0200

    option settings instead

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,6 @@ set -e
 
 export PATH="/root/.ebcli-virtual-env/executables:$PATH"
 
-eb config put $ENVIRONMENT
-
 eb deploy $ENVIRONMENT
 
-aws elasticbeanstalk update-environment --environment-name $ENVIRONMENT --application-name $APPLICATION --region $AWS_REGION --template-name $ENVIRONMENT
+aws elasticbeanstalk update-environment --environment-name $ENVIRONMENT --application-name $APPLICATION --region $AWS_REGION --option-settings file://$PWD/.optionsettings/$ENVIRONMENT.json
