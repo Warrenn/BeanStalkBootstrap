e28445e988fdd063d73c1fc5137a93b266edaab4
Warrenn Enslin <warrennenslin@Warrenns-MacBook-Pro.local>
2020-02-23 11:49:48 +0200

    trying to not use default

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,12 +13,12 @@ get_parameter() {
 # AWS_SESSION_TOKEN="${AWS_SESSION_TOKEN:-$(grep -Po '.*"Token".*' Iam.json | sed 's/ //g' | sed 's/"//g' | sed 's/,//g' | sed 's/Token://g')}"
 # rm Iam.json -f
 
-export DATABASE_URL="${DATABASE_URL:-$(get_parameter "DATABASE_URL")}"
-export RAILS_MASTER_KEY="${RAILS_MASTER_KEY:-$(get_parameter "RAILS_MASTER_KEY")}"
-export SECRET_KEY_BASE="${SECRET_KEY_BASE:-$(get_parameter "SECRET_KEY_BASE")}"
-export PUBLISHABLE_KEY="${PUBLISHABLE_KEY:-$(get_parameter "PUBLISHABLE_KEY")}"
-export SECRET_KEY="${SECRET_KEY:-$(get_parameter "SECRET_KEY")}"
-export MAILGUN_API_KEY="${MAILGUN_API_KEY:-$(get_parameter "MAILGUN_API_KEY")}"
+export DATABASE_URL="$(get_parameter "DATABASE_URL")"
+export RAILS_MASTER_KEY="$(get_parameter "RAILS_MASTER_KEY")"
+export SECRET_KEY_BASE="$(get_parameter "SECRET_KEY_BASE")"
+export PUBLISHABLE_KEY="$(get_parameter "PUBLISHABLE_KEY")"
+export SECRET_KEY="$(get_parameter "SECRET_KEY")"
+export MAILGUN_API_KEY="$(get_parameter "MAILGUN_API_KEY")"
 
 # Remove a potentially pre-existing server.pid for Rails.
 rm -rf /vbio/tmp/pids/server.pid
