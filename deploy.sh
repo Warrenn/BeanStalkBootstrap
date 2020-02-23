4f86f93cad83729b3c0787019bf9770dc030630c
Warrenn Enslin <warrennenslin@Warrenns-MacBook-Pro.local>
2020-02-23 18:48:44 +0200

    added the testing config

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,12 @@ set -e
 
 export PATH="/root/.ebcli-virtual-env/executables:$PATH"
 
+eb config put $ENVIRONMENT
+
 eb deploy $ENVIRONMENT
+
+aws elasticbeanstalk update-environment \
+    --environment-name $ENVIRONMENT \ 
+    --application-name $APPLICATION \
+    --region $AWS_REGION \
+    --template-name $ENVIRONMENT
